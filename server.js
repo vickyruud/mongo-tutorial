0a7eaaf46bfe3c1a5945c800e62beb293b8ba639
vickyruud <aprajit.k@gmail.com>
2022-04-13 14:25:41 -0400

    add body parser and test and install mongodb

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const bodyParser= require('body-parser')
 const app = express();
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.listen(5000, () => {
-  console.log("Happy Bday Maloo!!");
+  console.log("Listening on port 5000");
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  console.log(__dirname);
+})
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body)
 })
