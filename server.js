62c500b61fad7439b81b17bce0b23e8f62bba67d
vickyruud <aprajit.k@gmail.com>
2022-04-13 14:47:34 -0400

    add connection to mongo db and create a post route

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 const express = require("express");
 const bodyParser= require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient
 
 //body parser
-app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.listen(5000, () => {
   console.log("Listening on port 5000");
 })
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-  console.log(__dirname);
-})
+//connect to mongo db
+const connectionString = 'mongodb+srv://vickyruud:Ok78JCHRcgBknvxZ@firstcluster.0jwz1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+MongoClient.connect(connectionString)
+  .then(client => {
+    console.log('Connected to MongoDB');
+    const db = client.db('star-wars-quotes');
+    const quotesCollection = db.collection('quotes');
 
-app.post('/quotes', (req, res) => {
-  console.log(req.body)
-})
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html')
+      console.log(__dirname);
+      
+    });
+    app.post('/quotes', (req, res) => {
+      quotesCollection.insertOne(req.body)
+        .then(result => {
+          res.redirect('/')
+        })
+        .catch(error => console.log(error));
+    })
+    
+
+
+  })
+  .catch(error => (console.log(console.log(error))));
+
